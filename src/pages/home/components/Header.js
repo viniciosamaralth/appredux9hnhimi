09e3e7be08c8c9fe22bb9d24d9b711814b491fd9
Vinicios Amaral <vinicios.amaral@agilsoft.local>
2021-08-16 13:45:00 -0300

    https://www.youtube.com/watch?v=LGf2KJg20lg TEMPO 40:00

--- src/pages/home/components/Header.js
+++ src/pages/home/components/Header.js
@@ -1,23 +1,22 @@
-import React from 'react';
-import { Button} from '@material-ui/core';
-
+import React from "react";
+import { Button } from "@material-ui/core";
 
 function Header() {
-    return (
-        <header className="header">
-        <div className="toolbar">
-             <div>
-                <span>PROGRAMA DO VINI</span>
-             </div>
-            
-            <div>
-                <Button color='primary'>Novo Post</Button>
-                <span>img1</span>
-                <span>img2</span>
-            </div>
-        </div>
+  return (
+    <header className="header">
+      <div>
+        <span>PROGRAMA DO VINI</span>
+      </div>
+
+      <div>
+        <Button color="primary" variant="contained">
+          Novo Post
+        </Button>
+        <span>img1</span>
+        <span>img2</span>
+      </div>
     </header>
-    )
+  );
 }
 
 export default Header;
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,25 +1,29 @@
-import React from 'react';
-import './style.css'
-import Header from './components/Header';
+import React from "react";
+//import './style.css'
+import Header from "./components/Header";
+import { makeStyles } from "@material-ui/styles";
+
+const useStyle = makeStyles({
+  root: {
+    display: "flex",
+    flexDirection: "column",
+  },
+});
 
 function Home() {
-    return ( 
-        <div>
+  const classes = useStyle();
 
-          <Header/>
+  return (
+    <div className={classes.root}>
+      <Header />
 
-
-            <main className='main'>
-                <div className='navbar'>
-                    navbar
-                </div>
-                <div className='feed'>
-                    feed
-                </div>
-            </main>
-        </div>
-    );
-      
-};
+      <div className="toolbar"></div>
+      <main className="main">
+        <div className="navbar">navbar</div>
+        <div className="feed">feed</div>
+      </main>
+    </div>
+  );
+}
 
 export default Home;
