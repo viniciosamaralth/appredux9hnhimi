c3e3d33c61c69895c63748c3447fd6475184ba8b
william neves gato <vinicios.amaralth@gmail.com>
2021-10-08 15:00:57 -0300

    TEMPO 05:0500 https://www.youtube.com/watch?v=LGf2KJg20lg&t=2564s

--- src/pages/home/components/Header/Notifications.js
+++ src/pages/home/components/Header/Notifications.js
@@ -1,6 +1,18 @@
 import React, { useRef, useState, useEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
 import { useSelector, useDispatch } from "react-redux";
-import { SvgIcon, IconButton, Popover } from "@material-ui/core/";
+import {
+  SvgIcon,
+  IconButton,
+  Popover,
+  Typography,
+  Box,
+  List,
+  ListItem,
+  ListItemText,
+  ListItemAvatar,
+  Avatar,
+} from "@material-ui/core/";
 import {
   Bell as BellIcon,
   Star as StarIcon,
@@ -10,13 +22,30 @@ import {
 } from "react-feather";
 import { getNotifications } from "../../../../store/actions/notificationsActions";
 
+const iconsMap = {
+  reviews: StarIcon,
+  new_comment: MessageIcon,
+  like: HeartIcon,
+  connection: ConnectionIcon,
+};
+
+const useStyle = makeStyles((theme) => ({
+  icon: {
+    background: theme.palette.secondary.main,
+    color: theme.palette.secondary.contrastText,
+  },
+}));
+
 function Notification() {
   const account = useSelector((state) => state.account);
-  const notifications = useSelector((state) => state.notifications);
+  const notifications = useSelector(
+    (state) => state.notifications.notifications
+  );
   const isAuthenticated = !!account.user;
   const ref = useRef(null);
   const [isOpen, setOpen] = useState(false);
   const dispatch = useDispatch();
+  const classes = useStyle();
 
   const handleOpen = () => {
     setOpen(true);
@@ -30,14 +59,16 @@ function Notification() {
     dispatch(getNotifications());
   }, [dispatch]);
 
-  console.log(notifications);
+  useEffect(() => {
+    console.log(notifications);
+  }, [notifications]);
 
   return (
     isAuthenticated && (
       <>
         <IconButton ref={ref} onClick={handleOpen}>
           <SvgIcon>
-            <Bell />
+            <BellIcon />
           </SvgIcon>
         </IconButton>
         <Popover
@@ -53,7 +84,36 @@ function Notification() {
             horizontal: "right",
           }}
         >
-          The content of the Popover.
+          <Box p={2}>
+            <Typography variant="h6" color="textPrimary">
+              Notificações
+            </Typography>
+          </Box>
+
+          <List>
+            {notifications.map((notification) => {
+              const Icon = iconsMap[notification.type];
+              return (
+                <ListItem>
+                  <ListItemAvatar>
+                    <Avatar className={classes.icon}>
+                      <SvgIcon>
+                        <Icon />
+                      </SvgIcon>
+                    </Avatar>
+                  </ListItemAvatar>
+                  <ListItemText
+                    primary={notification.title}
+                    primaryTypographyProps={{
+                      variant: "subtitle2",
+                      color: "textPrimary",
+                    }}
+                    secondary={notification.description}
+                  />
+                </ListItem>
+              );
+            })}
+          </List>
         </Popover>
       </>
     )
